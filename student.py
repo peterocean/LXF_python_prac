65d21f6a7ed5a88e513d324a184f47c2c5fc075d
peter yang <yyf10601@163.com>
2017-08-04 23:13:50 +0800

    1: 练习@property

--- student.py
+++ student.py
@@ -1,15 +1,47 @@
 #!/usr/bin/env python3
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*_
 
 class Student(object):
-    
-    def __init__(self, name, score):
-        self.__name = name
-        self.__score = score
-    pass
-    
-    def print_score(self):
-        print('%s:%s' %(self.__name, self.__score))
+    def __init__(self):
+        self.__score = 0
+    def get_score(self):
+        return self.__score
+    def set_score(self,value):
+        if not isinstance(value,int):
+            raise ValueError("score must be intger!")
+        if value < 0 or value > 100:
+            raise ValueError("score must between 0~100!")
+        self.__score = value
+        
+    @property
+    def birth(self):
+        return self.__birth
+    @birth.setter
+    def birth(self,value):
+        self.__birth = value
 
-bart = Student('Bart Simpson', 98)
-bart.print_score()
+    @property
+    def age(self):
+        return 2015-self.__birth
+    
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self,new_name):
+        if not isinstance(new_name,str):
+            raise ValueError("name must be string!")
+        self.__name = new_name
+    @name.getter
+    def name(self):
+        return self.__name
+    
+s = Student()
+s.name = "peter yang"
+print(s.name)
+print(s.get_score())
+s.set_score(60)
+print(s.get_score())
+s.name = 60
+
+                             
